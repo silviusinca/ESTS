b5ff8bfa040e454a3583bbd0da6ed0e7d4323d65
Stefan Magureanu <stefanmagureanu25@gmail.com>
2023-04-21 01:54:28 +0300

    Almost done ViolettaBedroom

---
+++ Assets/Scripts/ViolettaScripts/Luminite.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class Luminite : MonoBehaviour
+{
+    [SerializeField] GameObject visualEffects;
+    Vector2 mouse;
+    void Start()
+    {
+        visualEffects.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            visualEffects.SetActive(true);
+            visualEffects.transform.position = new Vector3(mouse.x, mouse.y, 0f);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            visualEffects.SetActive(false);
+        }
+    }
+}
