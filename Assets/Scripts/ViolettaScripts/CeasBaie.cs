f5f02b7d1af5cd32330ec5f9defe612d5fd39158
Cosmina <cosmina-elena.duca@s.unibuc.ro>
2023-06-04 22:01:41 +0300

    ceasu cu zoom

---
+++ Assets/Scripts/ViolettaScripts/CeasBaie.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CeasBaie : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        SceneManager.LoadScene("CeasCada");
+    }
+}
